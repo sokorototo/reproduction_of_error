4635fb0dc7c26766cc72d7545aac44f0411fdb7e
sokorototo <nyachiengatoto@gmail.com>
2022-02-09 03:48:37 +0300

    more random

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use std::{
 };
 
 // The data in transit
-const DATA: [u8; 200] = *b"01234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789";
+const DATA: [u8; 200] = *b"5m4IreHCOsr9jGyKAIP5z1GmH40GKcJY6O0WzelRKpu8r3A8bHKXDxx4ka5cRc92qviKCe9t1E0PHfPb7Bovr0WEjL4BdrIpfSN4Vu1SCA0ivBpdb9j7K9MDH6NE6EwU2wTZTPHegzf2oJ9fk2Jb053H6t5YvgzT2BfsjP2j1KyVTzDwApzTVV7jhAfWPvreX7bHWbkH";
 
 // A reader that can be sent over threads as it is both `Send` and `Sync`
 struct Reader<T: Read + Seek>(Arc<Mutex<T>>);
@@ -28,7 +28,7 @@ impl<T: Read + Seek> Reader<T> {
             guard.read_exact(raw.as_mut_slice()).unwrap();
         }
 
-        let region = (entry.location) as usize..(entry.location as usize + entry.offset);
+        let region = (entry.location) as usize..entry.location as usize + entry.offset;
         assert_eq!(&raw, &DATA[region]);
 
         raw
